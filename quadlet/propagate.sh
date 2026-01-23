446d39aaef4805a1d4d1a27239a05b8ec467432b
Patryk Grabowski <grabowskip@icloud.com>
2026-01-23 01:51:42 +0100

    feat: extended propagate script

--- quadlet/propagate.sh
+++ quadlet/propagate.sh
@@ -2,5 +2,15 @@
 
 TARGET_PATH=/usr/share/containers/systemd/
 
+echo "Creating a directory $TARGET_PATH"
 mkdir -p $TARGET_PATH
+
+echo "Copying *.container files"
 cp *.container $TARGET_PATH
+echo "Files copied"
+
+echo "Listing files:"
+ls -la $TARGET_PATH
+
+echo "Reloading systemd daemon"
+systemctl --user daemon-reload
